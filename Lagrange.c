2d76058494a5c495091d24c699324df03c88c805
Rahul Kumar Singh <87928472+rahulsingh7111711@users.noreply.github.com>
2024-11-27 15:42:20 +0530

    Update Lagrange.c

--- Lagrange.c
+++ Lagrange.c
@@ -1,50 +1,51 @@
-#include <stdio.h>
-double lagrangeInterpolation(double x[], double y[], int n, double target)
-{
-    double result = 0.0;
-    for (int i = 0; i < n; i++)
-    {
-        double term = y[i];
-        for (int j = 0; j < n; j++)
-        {
-            if (j != i)
-            {
-                term *= (target - x[j]) / (x[i] - x[j]);
-            }
-        }
-        result += term;
-    }
-    return result;
-}
+#include <stdio.h> 
+#define MAX_SIZE 100 
 
-int main()
-{
-    int n;
+int main() 
+{ 
+    int n; 
     printf("Name: MANSI AGARWAL \nRoll No: 11 \nSection: C\n\n");
-    printf("Enter the number of points: ");
-    scanf("%d", &n);
-    double x[n], y[n];
-    printf("Enter the values of x and y:\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("x[%d] = ", i);
-        scanf("%lf", &x[i]);
-        printf("y[%d] = ", i);
-        scanf("%lf", &y[i]);
-    }
-    double k;
-    printf("Enter the value of x to calculate y for : ");
-    scanf("%lf", &k);
-    int isUniform = 1;
-    for (int i = 1; i < n; i++)
-    {
-        if (x[i] != x[0])
-        {
-            isUniform = 0;
-            break;
-        }
-    }
-    double result = lagrangeInterpolation(x, y, n, k);
-    printf("The value of y at x = %.2lf is %.5lf\n", k, result);
+    printf("Enter number of terms: "); 
+    scanf("%d", &n); 
+
+    if (n > MAX_SIZE) 
+    { 
+        printf("The number of terms exceeds the maximum allowed size.\n"); 
+        return 1; 
+    } 
+    
+    double X[MAX_SIZE], Y[MAX_SIZE], x, sum = 0, term; 
+    int i, j; 
+
+    printf("Enter values of X:\n"); 
+    for (i = 0; i < n; i++) 
+    { 
+        scanf("%lf", &X[i]); 
+    } 
+    
+    printf("Enter values of Y:\n"); 
+    for (i = 0; i < n; i++) 
+    { 
+        scanf("%lf", &Y[i]); 
+    } 
+    
+    printf("Enter the value of x for which you want y: "); 
+    scanf("%lf", &x); 
+    
+    for (i = 0; i < n; i++) 
+    { 
+        term = 1; 
+        for (j = 0; j < n; j++) 
+        { 
+            if (i != j) 
+            { 
+                term *= (x - X[j]) / (X[i] - X[j]); 
+            } 
+        } 
+        sum += term * Y[i]; 
+    } 
+    
+    printf("\nValue at X = %lf is Y = %lf\n", x, sum); 
+    
     return 0;
 }
