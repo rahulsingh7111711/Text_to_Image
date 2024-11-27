dd45f22dcd504fa53d7081d71f4101d2937a04e7
Rahul Kumar Singh <87928472+rahulsingh7111711@users.noreply.github.com>
2024-11-27 15:45:47 +0530

    Update Least_Sq_Mean.c

--- Least_Sq_Mean.c
+++ Least_Sq_Mean.c
@@ -1,72 +1,81 @@
 #include <stdio.h>
-void gaussJordan(float a[2][3], int n) 
+#define S 50
+
+int main() 
 {
-    float ratio;
-    for(int i = 0; i < n; i++) {
-        if(a[i][i] == 0.0) {
-            printf("Mathematical Error!");
-            return;
+    int n, i;
+    float x[S], y[S], sumX = 0, sumX2 = 0, sumY = 0, sumXY = 0, a, b;
+
+    printf("Name: MANSI AGARWAL \nRoll No: 11 \nSection: C\n\n");
+    
+    // Input number of data points
+    printf("Number of data points: ");
+    scanf("%d", &n);
+
+    // Input data points for X
+    printf("Enter data X: ");
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%f", &x[i]); 
+    }
+
+    // Input data points for Y
+    printf("Enter data Y: ");
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%f", &y[i]);
+    }
+    
+    // Calculate sums for the normal equations
+    for (i = 0; i < n; i++) 
+    {
+        sumX += x[i];
+        sumX2 += x[i] * x[i];
+        sumY += y[i];
+        sumXY += x[i] * y[i];
+    }
+
+    // Display the normal equations
+    printf("\nEquations in the form of a and b: \n");
+    printf("%da + %.2fb = %.2f\n", n, sumX, sumY);
+    printf("%.2fa + %.2fb = %.2f\n", sumX, sumX2, sumXY);
+
+    // Augmented matrix for the system of equations
+    float matrix[2][3] = {
+        { n, sumX, sumY },
+        { sumX, sumX2, sumXY }
+    };
+
+    // Applying Gauss-Jordan elimination
+    for (i = 0; i < 2; i++) 
+    {
+        float divisor = matrix[i][i];
+        for (int j = 0; j < 3; j++) 
+        {
+            matrix[i][j] /= divisor;
         }
-        for(int j = 0; j < n; j++) {
-            if(i != j) {
-                ratio = a[j][i] / a[i][i];
-                for(int k = 0; k < n + 1; k++) {
-                    a[j][k] = a[j][k] - ratio * a[i][k];
+
+        // Eliminate the current column from the other row
+        for (int k = 0; k < 2; k++) 
+        {
+            if (k != i)
+            {
+                float factor = matrix[k][i];
+                for (int j = 0; j < 3; j++) 
+                {
+                    matrix[k][j] -= factor * matrix[i][j];
                 }
             }
         }
     }
-}
 
-int main() {
-    int n;
-    printf("Name: MANSI AGARWAL \nRoll No: 11 \nSection: C\n\n");
-    
-    printf("Input the number of data points: ");
-    scanf("%d", &n);
+    // Extracting the solution from the augmented matrix
+    a = matrix[0][2];
+    b = matrix[1][2];
 
-    if (n < 2) {
-        printf("At least two data points are required.\n");
-        return -1;
-    }
+    // Output the results
+    printf("Values are: a = %.2f and b = %.2f\n", a, b);
+    printf("Final obtained equation is: y = %.2f + %.2fx\n", a, b);
 
-    float x[n], y[n];
-    float sxy = 0, sx = 0, sy = 0, sx2 = 0;
-
-    // Input x values
-    printf("Input the values of x:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%f", &x[i]);
-    }
-
-    // Input y values
-    printf("Input the values of y:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%f", &y[i]);
-    }
-    for (int i = 0; i < n; i++) 
-    {
-        sx += x[i];           
-        sy += y[i];           
-        sx2 += x[i] * x[i];   
-        sxy += x[i] * y[i];   
-    }
-
-    float m[2][3];
-    m[0][0] = n;            
-    m[0][1] = sx;           
-    m[0][2] = sy;           
-    m[1][0] = sx;           
-    m[1][1] = sx2;          
-    m[1][2] = sxy;         
-    printf("\nApply gauss jordan : \n");
-    printf("Equation 1 : %0.2fa + %0.2fb = %0.2f\n",m[0][0],m[0][1],m[0][2]);
-    printf("Equation 2 : %0.2fa + %0.2fb = %0.2f\n\n",m[1][0],m[1][1],m[1][2]);
-    gaussJordan(m, 2);
-
-    float b = m[1][2]/m[1][1];  
-    float a = m[0][2]/m[0][0];
-    printf("\nValue a : %.2f and b : %.2f\n", a, b);
-    printf("Equation : y = %0.2fx + %0.2f\n",b,a);
     return 0;
 }
